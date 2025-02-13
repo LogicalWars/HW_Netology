da4bbdb53686fb35b60b37e2cd9fd8a1eec08897
KoshelevVA <koshelevva96@gmail.com>
2025-02-13 20:29:25 +0400

    done2

--- HW_ABST/Second/src/Main.java
+++ HW_ABST/Second/src/Main.java
@@ -1,5 +1,18 @@
+import logger.SimpleLogger;
+import logger.SmartLogger;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SimpleLogger simpleLogger = new SimpleLogger();
+        SmartLogger smartLogger = new SmartLogger();
+
+        simpleLogger.log("test log");
+        simpleLogger.log("test log1");
+        simpleLogger.log("test log2");
+
+        smartLogger.log("test log error");
+        smartLogger.log("test log ERROR1");
+        smartLogger.log("test log2");
+        smartLogger.log("test log3");
     }
 }
---
+++ HW_ABST/Second/src/logger/SmartLogger.java
@@ -0,0 +1,20 @@
+package logger;
+
+import java.time.LocalDateTime;
+
+public class SmartLogger implements Logger{
+
+    private int count;
+
+    @Override
+    public void log(String msg) {
+        count++;
+        String logLevel;
+        if (msg.toLowerCase().contains("error")){
+            logLevel = "ERROR";
+        } else {
+            logLevel = "INFO";
+        }
+        System.out.println(logLevel+"#"+count+" "+"["+LocalDateTime.now()+"]"+" "+msg);
+    }
+}
